9ab670f584815ad2e41180003574e8ccde82d312
e.ivanchenko <e.ivanchenko@dodopizza.com>
2021-04-08 09:25:11 +0300

    Add random coordinates pair for MapsClientMock

--- DILearn/MapsClientMock.cs
+++ DILearn/MapsClientMock.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Security.Cryptography;
 using System.Threading.Tasks;
 
 namespace DILearn
@@ -6,7 +8,19 @@ namespace DILearn
     {
         public Task<Coordinates> GetAddressCoordinates(string address)
         {
-            return Task.FromResult(new Coordinates {Longitude = 10, Latitude = 12});
+            var coordinatesPool = new List<Coordinates>
+            {
+                new() {Longitude = 10, Latitude = 12},
+                new() {Longitude = 180, Latitude = 90},
+                new() {Longitude = 0, Latitude = 0},
+                new() {Longitude = 17, Latitude = 62},
+                new() {Longitude = 1, Latitude = 90},
+                new() {Longitude = 180, Latitude = 1},
+                new() {Longitude = 0, Latitude = 30},
+                new() {Longitude = 117, Latitude = 62}
+            };
+            var random = RandomNumberGenerator.GetInt32(0, 7);
+            return Task.FromResult(coordinatesPool[random]);
         }
     }
 }
